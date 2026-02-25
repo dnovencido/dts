f931f6202690b87696529b6ffaea22b2a29e3c14
Denver Novencido <dnovencido@gmail.com>
2026-02-25 20:55:01 +0800

    Change datepicker value

--- src/assets/js/date_range.js
+++ src/assets/js/date_range.js
@@ -1,16 +1,36 @@
 $(function() {
-    // var start = [moment(), moment()][0];
-    // var end = moment();
     $('.date-range').daterangepicker({
-        // startDate: start,
-        // endDate: end,
+        autoUpdateInput: false,   // 🔥 prevents default date
+        opens: 'right',
         ranges: {
            'Today': [moment(), moment()],
            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
            'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+           'Last Month': [
+               moment().subtract(1, 'month').startOf('month'),
+               moment().subtract(1, 'month').endOf('month')
+           ]
+        },
+
+        locale: {
+            cancelLabel: 'Clear'
         }
-    })
+    });
+
+    // When user selects a range
+    $('.date-range').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(
+            picker.startDate.format('MM/DD/YYYY') +
+            ' - ' +
+            picker.endDate.format('MM/DD/YYYY')
+        );
+    });
+
+    // When user clicks Clear
+    $('.date-range').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    });
+
 });
